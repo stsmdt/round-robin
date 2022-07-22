faf59d1e932ccbfaee5f161d8ea0b10e0f7ba6fb
Steffen Schmidt <git@brombeersaft.com>
2022-07-22 18:44:46 +0200

    chore: initial source

---
+++ round_robin.go
@@ -0,0 +1,48 @@
+package roundrobin
+
+import (
+	"errors"
+	"net/url"
+	"sync/atomic"
+)
+
+// RoundRobin is an interface which represents the round-robin balancing.
+type RoundRobin interface {
+	Next() *url.URL
+}
+
+type roundrobin struct {
+	urls      []*url.URL
+	nextIndex uint32
+}
+
+// New returns a RoundRobin implementation
+func New(urls ...*url.URL) (RoundRobin, error) {
+	if len(urls) == 0 {
+		return nil, errors.New("no urls provided")
+	}
+
+	return &roundrobin{
+		urls: urls,
+	}, nil
+}
+
+// Next returns the next url
+func (r *roundrobin) Next() *url.URL {
+	var next uint32
+
+	for {
+		prev := atomic.LoadUint32(&r.nextIndex)
+		next = prev + 1
+
+		if next > uint32(len(r.urls)) {
+			next = 1
+		}
+
+		if atomic.CompareAndSwapUint32(&r.nextIndex, prev, next) {
+			break
+		}
+	}
+
+	return r.urls[next-1]
+}
