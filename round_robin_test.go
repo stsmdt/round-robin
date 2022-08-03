06c43f7cc743f60366f68c22b99bde3aa2742f9c
Steffen Schmidt <git@brombeersaft.com>
2022-08-03 15:31:29 +0200

    refactor: remove github.com/stretchr/testify/assert dependency

--- round_robin_test.go
+++ round_robin_test.go
@@ -1,26 +1,25 @@
 package roundrobin_test
 
 import (
-	"errors"
 	"net/url"
+	"reflect"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	roundrobin "github.com/stsmdt/round-robin"
 )
 
 func TestRoundRobin(t *testing.T) {
-	tests := []struct {
-		desc          string
-		urls          []*url.URL
-		expected      []*url.URL
-		expectedError error
+	testcases := []struct {
+		desc        string
+		urls        []*url.URL
+		expected    []*url.URL
+		expectedErr string
 	}{
 		{
-			desc:          "No urls provided",
-			urls:          []*url.URL{},
-			expected:      []*url.URL{},
-			expectedError: errors.New("no urls provided"),
+			desc:        "No urls provided",
+			urls:        []*url.URL{},
+			expected:    []*url.URL{},
+			expectedErr: "no urls provided",
 		},
 		{
 			desc: "Everything provided",
@@ -44,20 +43,25 @@ func TestRoundRobin(t *testing.T) {
 		},
 	}
 
-	for _, test := range tests {
-		t.Run(test.desc, func(t *testing.T) {
-			roundRobin, err := roundrobin.New(test.urls...)
+	for _, tc := range testcases {
+		t.Run(tc.desc, func(t *testing.T) {
+			roundRobin, err := roundrobin.New(tc.urls...)
 
-			gots := make([]*url.URL, 0, len(test.expected))
-			for j := 0; j < len(test.expected); j++ {
+			gots := make([]*url.URL, 0, len(tc.expected))
+			for j := 0; j < len(tc.expected); j++ {
 				gots = append(gots, roundRobin.Next())
 			}
 
-			if test.expectedError != nil && assert.Error(t, err) {
-				assert.Equal(t, test.expectedError, err)
+			switch {
+			case err == nil && len(tc.expectedErr) > 0:
+				t.Errorf("Expected error %q but got nil", tc.expectedErr)
+			case err != nil && err.Error() != tc.expectedErr:
+				t.Errorf("Expected error %q but got %q", tc.expectedErr, err.Error())
 			}
 
-			assert.Equal(t, test.expected, gots)
+			if got, want := gots, tc.expected; !reflect.DeepEqual(got, want) {
+				t.Errorf("Expected %v but got %v", want, got)
+			}
 		})
 	}
 }
