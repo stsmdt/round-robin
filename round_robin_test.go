30debefdda5a59727271065b8cf9858a70c29c0a
Steffen Schmidt <git@brombeersaft.com>
2022-08-03 15:31:29 +0200

    test: add benchmark tests

--- round_robin_test.go
+++ round_robin_test.go
@@ -1,8 +1,10 @@
 package roundrobin_test
 
 import (
+	"fmt"
 	"net/url"
 	"reflect"
+	"sync"
 	"testing"
 
 	roundrobin "github.com/stsmdt/round-robin"
@@ -62,6 +64,81 @@ func TestRoundRobin(t *testing.T) {
 			if got, want := gots, tc.expected; !reflect.DeepEqual(got, want) {
 				t.Errorf("Expected %v but got %v", want, got)
 			}
+		})
+	}
+}
+
+func BenchmarkRoundRobinSync(b *testing.B) {
+	urls := []*url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+		{Host: "127.0.0.6"},
+		{Host: "127.0.0.7"},
+		{Host: "127.0.0.8"},
+		{Host: "127.0.0.9"},
+		{Host: "127.0.0.10"},
+		{Host: "127.0.0.11"},
+		{Host: "127.0.0.12"},
+		{Host: "127.0.0.13"},
+		{Host: "127.0.0.14"},
+		{Host: "127.0.0.15"},
+	}
+
+	for i := 1; i <= len(urls); i++ {
+		b.Run(fmt.Sprintf("RoundRobinSliceOf(%d)", i), func(b *testing.B) {
+			roundrobin, err := roundrobin.New(urls[:i]...)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			waitGroup := &sync.WaitGroup{}
+			for i := 0; i < b.N; i++ {
+				waitGroup.Add(1)
+				defer waitGroup.Done()
+				roundrobin.Next()
+			}
+		})
+	}
+}
+
+func BenchmarkRoundRobinAsync(b *testing.B) {
+	urls := []*url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+		{Host: "127.0.0.6"},
+		{Host: "127.0.0.7"},
+		{Host: "127.0.0.8"},
+		{Host: "127.0.0.9"},
+		{Host: "127.0.0.10"},
+		{Host: "127.0.0.11"},
+		{Host: "127.0.0.12"},
+		{Host: "127.0.0.13"},
+		{Host: "127.0.0.14"},
+		{Host: "127.0.0.15"},
+	}
+
+	for i := 1; i <= len(urls); i++ {
+		b.Run(fmt.Sprintf("RoundRobinSliceOf(%d)", i), func(b *testing.B) {
+			roundrobin, err := roundrobin.New(urls[:i]...)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			waitGroup := &sync.WaitGroup{}
+			for i := 0; i < b.N; i++ {
+				waitGroup.Add(1)
+				go func() {
+					defer waitGroup.Done()
+					roundrobin.Next()
+				}()
+			}
+			waitGroup.Wait()
 		})
 	}
 }
