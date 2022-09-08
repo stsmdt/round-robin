5751121b6dc6e3f9d61216176e30db66621eb382
Steffen Schmidt <git@brombeersaft.com>
2022-09-08 15:19:24 +0200

    feat: switch from pointer to value slices
    BREAKING CHANGE: value slices are now used instead of pointer slices

--- round_robin.go
+++ round_robin.go
@@ -9,29 +9,32 @@ import (
 
 // RoundRobin is an interface which represents the round-robin balancing.
 type RoundRobin interface {
-	Next() *url.URL
+	Next() url.URL
 }
 
 type roundrobin struct {
-	urls  []*url.URL
+	urls  []url.URL
 	index atomic.Uint32
 }
 
+// ErrNoURLsProvided is the error that no URLs were provided
 var ErrNoURLsProvided = errors.New("no urls provided")
 
 // New returns a RoundRobin implementation
-func New(urls ...*url.URL) (RoundRobin, error) {
+func New(urls []url.URL) (RoundRobin, error) {
 	if len(urls) == 0 {
 		return nil, ErrNoURLsProvided
 	}
 
-	return &roundrobin{
-		urls: urls,
-	}, nil
+	rr := &roundrobin{}
+	rr.urls = make([]url.URL, len(urls))
+	copy(rr.urls, urls)
+
+	return rr, nil
 }
 
 // Next returns the next url
-func (r *roundrobin) Next() *url.URL {
+func (r *roundrobin) Next() url.URL {
 	var next uint32
 
 	for {
--- round_robin_test.go
+++ round_robin_test.go
@@ -3,73 +3,138 @@ package roundrobin_test
 import (
 	"fmt"
 	"net/url"
-	"reflect"
 	"sync"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	roundrobin "github.com/stsmdt/round-robin"
 )
 
-func TestRoundRobin(t *testing.T) {
-	testcases := []struct {
-		desc        string
-		urls        []*url.URL
-		expected    []*url.URL
-		expectedErr string
-	}{
-		{
-			desc:        "No urls provided",
-			urls:        []*url.URL{},
-			expected:    []*url.URL{},
-			expectedErr: "no urls provided",
-		},
-		{
-			desc: "Everything provided",
-			urls: []*url.URL{
-				{Host: "127.0.0.1"},
-				{Host: "127.0.0.2"},
-				{Host: "127.0.0.3"},
-				{Host: "127.0.0.4"},
-			},
-			expected: []*url.URL{
-				{Host: "127.0.0.1"},
-				{Host: "127.0.0.2"},
-				{Host: "127.0.0.3"},
-				{Host: "127.0.0.4"},
-				{Host: "127.0.0.1"},
-				{Host: "127.0.0.2"},
-				{Host: "127.0.0.3"},
-				{Host: "127.0.0.4"},
-				{Host: "127.0.0.1"},
-			},
-		},
-	}
+func TestNew_NoURLsProvided(t *testing.T) {
+	a := assert.New(t)
 
-	for _, tc := range testcases {
-		t.Run(tc.desc, func(t *testing.T) {
-			roundRobin, err := roundrobin.New(tc.urls...)
-
-			gots := make([]*url.URL, 0, len(tc.expected))
-			for j := 0; j < len(tc.expected); j++ {
-				gots = append(gots, roundRobin.Next())
-			}
-
-			switch {
-			case err == nil && len(tc.expectedErr) > 0:
-				t.Errorf("Expected error %q but got nil", tc.expectedErr)
-			case err != nil && err.Error() != tc.expectedErr:
-				t.Errorf("Expected error %q but got %q", tc.expectedErr, err.Error())
-			}
-
-			if got, want := gots, tc.expected; !reflect.DeepEqual(got, want) {
-				t.Errorf("Expected %v but got %v", want, got)
-			}
-		})
-	}
+	rr, err := roundrobin.New([]url.URL{})
+	a.Nil(rr)
+	a.Error(err)
+	a.ErrorIs(err, roundrobin.ErrNoURLsProvided)
 }
 
-func BenchmarkRoundRobinSync(b *testing.B) {
-	urls := []*url.URL{
+func TestNew_URLsProvided(t *testing.T) {
+	a := assert.New(t)
+
+	rr, err := roundrobin.New([]url.URL{{Host: "127.0.0.1"}})
+	a.NotNil(rr)
+	a.NoError(err)
+}
+
+func TestNext_NoMutation(t *testing.T) {
+	a := assert.New(t)
+
+	givenURLs := []url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+	}
+	wantURLs := []url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+	}
+
+	rr, err := roundrobin.New(givenURLs)
+
+	gotURLs := make([]url.URL, 0, len(wantURLs))
+	for i := 0; i < len(wantURLs); i++ {
+		gotURLs = append(gotURLs, rr.Next())
+	}
+
+	a.NoError(err)
+	a.Equal(wantURLs, gotURLs)
+}
+
+func TestNext_GivenURLsMutation(t *testing.T) {
+	a := assert.New(t)
+
+	givenURLs := []url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+	}
+	wantURLs := []url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+	}
+
+	rr, err := roundrobin.New(givenURLs)
+
+	givenURLs[0] = url.URL{Host: "127.0.0.6"}
+
+	gotURLs := make([]url.URL, 0, len(wantURLs))
+	for i := 0; i < len(wantURLs); i++ {
+		gotURLs = append(gotURLs, rr.Next())
+	}
+
+	a.NoError(err)
+	a.Equal(wantURLs, gotURLs)
+}
+
+func TestNext_GotURLMutation(t *testing.T) {
+	a := assert.New(t)
+
+	givenURLs := []url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+	}
+	wantURLs := []url.URL{
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+		{Host: "127.0.0.1"},
+		{Host: "127.0.0.2"},
+		{Host: "127.0.0.3"},
+		{Host: "127.0.0.4"},
+		{Host: "127.0.0.5"},
+	}
+
+	rr, err := roundrobin.New(givenURLs)
+
+	gotURLs := make([]url.URL, 0, len(wantURLs))
+	for i := 0; i < len(wantURLs); i++ {
+		gotURL := rr.Next()
+		gotURLs = append(gotURLs, gotURL)
+		gotURL.Host = "127.0.0.6"
+	}
+
+	a.NoError(err)
+	a.Equal(wantURLs, gotURLs)
+}
+
+func BenchmarkNext_Sync(b *testing.B) {
+	givenURLs := []url.URL{
 		{Host: "127.0.0.1"},
 		{Host: "127.0.0.2"},
 		{Host: "127.0.0.3"},
@@ -80,32 +145,27 @@ func BenchmarkRoundRobinSync(b *testing.B) {
 		{Host: "127.0.0.8"},
 		{Host: "127.0.0.9"},
 		{Host: "127.0.0.10"},
-		{Host: "127.0.0.11"},
-		{Host: "127.0.0.12"},
-		{Host: "127.0.0.13"},
-		{Host: "127.0.0.14"},
-		{Host: "127.0.0.15"},
 	}
 
-	for i := 1; i <= len(urls); i++ {
-		b.Run(fmt.Sprintf("RoundRobinSliceOf(%d)", i), func(b *testing.B) {
-			roundrobin, err := roundrobin.New(urls[:i]...)
+	for i := 1; i <= len(givenURLs); i++ {
+		b.Run(fmt.Sprintf("slice_len_%d", i), func(b *testing.B) {
+			rr, err := roundrobin.New(givenURLs[:i])
 			if err != nil {
 				b.Fatal(err)
 			}
 
-			waitGroup := &sync.WaitGroup{}
+			wg := &sync.WaitGroup{}
 			for i := 0; i < b.N; i++ {
-				waitGroup.Add(1)
-				defer waitGroup.Done()
-				roundrobin.Next()
+				wg.Add(1)
+				defer wg.Done()
+				rr.Next()
 			}
 		})
 	}
 }
 
-func BenchmarkRoundRobinAsync(b *testing.B) {
-	urls := []*url.URL{
+func BenchmarkNext_Async(b *testing.B) {
+	givenURLs := []url.URL{
 		{Host: "127.0.0.1"},
 		{Host: "127.0.0.2"},
 		{Host: "127.0.0.3"},
@@ -116,29 +176,24 @@ func BenchmarkRoundRobinAsync(b *testing.B) {
 		{Host: "127.0.0.8"},
 		{Host: "127.0.0.9"},
 		{Host: "127.0.0.10"},
-		{Host: "127.0.0.11"},
-		{Host: "127.0.0.12"},
-		{Host: "127.0.0.13"},
-		{Host: "127.0.0.14"},
-		{Host: "127.0.0.15"},
 	}
 
-	for i := 1; i <= len(urls); i++ {
-		b.Run(fmt.Sprintf("RoundRobinSliceOf(%d)", i), func(b *testing.B) {
-			roundrobin, err := roundrobin.New(urls[:i]...)
+	for i := 1; i <= len(givenURLs); i++ {
+		b.Run(fmt.Sprintf("slice_len_%d", i), func(b *testing.B) {
+			rr, err := roundrobin.New(givenURLs[:i])
 			if err != nil {
 				b.Fatal(err)
 			}
 
-			waitGroup := &sync.WaitGroup{}
+			wg := &sync.WaitGroup{}
 			for i := 0; i < b.N; i++ {
-				waitGroup.Add(1)
+				wg.Add(1)
 				go func() {
-					defer waitGroup.Done()
-					roundrobin.Next()
+					defer wg.Done()
+					rr.Next()
 				}()
 			}
-			waitGroup.Wait()
+			wg.Wait()
 		})
 	}
 }
